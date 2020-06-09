3e15f1cf20c1ea457cbda36b8b409fbc42b8fafc
Jackson McComas <jackson.mccomas@gmail.com>
2020-06-09 14:33:32 -0500

    2nd pass complete

--- LeetCode/MissingNumber.js
+++ LeetCode/MissingNumber.js
@@ -19,3 +19,30 @@ var missingNumber = function (nums) {
   if (sorted[0] === 0) return sorted[sorted.length - 1] + 1;
   if (sorted[0] === 1) return sorted[0] - 1;
 };
+
+// console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]), 8);
+// console.log(missingNumber([3, 0, 1]), 2);
+
+// Populate hash map with 0 to nums.length as the keys. set them all to 0
+
+// loop though the nums array and add 1 to each key value
+
+// return key with value == 0
+
+var missingNumber2 = function (nums) {
+  let hash = {};
+  for (let i = 0; i <= nums.length; i++) {
+    hash[i] = 0;
+  }
+  nums.forEach((num) => {
+    hash[num] += 1;
+  });
+
+  // return key with value of 0
+  for (let i in hash) {
+    if (hash[i] === 0) return i;
+  }
+};
+
+console.log(missingNumber2([9, 6, 4, 2, 3, 5, 7, 0, 1]), 8);
+console.log(missingNumber2([3, 0, 1]), 2);
